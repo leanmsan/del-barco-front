ac8ccd181f9a46f6b6b50e1396e56b3117f821d2
Leandro Sánchez <103838718+leanmsan@users.noreply.github.com>
2023-10-12 11:45:19 -0300

    Tabla Proveedores funcionando, hay que hacer css

--- front/src/components/Proveedores/TablaProveedores.jsx
+++ front/src/components/Proveedores/TablaProveedores.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 import { TableContainer, Table, TableHead, TableRow, TableCell, TableBody, Paper } from '@mui/material';
 import axios from 'axios';
 
