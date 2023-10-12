b4b2d3c21948e24c6b41a71df6b0a56534883253
Leandro Sánchez <103838718+leanmsan@users.noreply.github.com>
2023-10-12 12:18:13 -0300

    .

--- front/src/pages/Menu.jsx
+++ front/src/pages/Menu.jsx
@@ -1,32 +1,27 @@
-import { useState } from 'react'
-import { useNavigate } from 'react-router-dom'
-import { SideBar } from '../components/SideBar'
-import { Inicio } from '../components/Inicio'
-import { TablaInsumos } from '../components/insumos/TablaInsumos'
-import { ProveedoresPage } from './ProveedoresPage'
-
-import '../css/menu.css'
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { SideBar } from "../components/SideBar";
+import { Inicio } from "../components/Inicio";
+import { TablaInsumos } from "../components/insumos/TablaInsumos";
+import { ProveedoresPage } from "./ProveedoresPage";
+import "../css/menu.css";
 
 export function Menu() {
-    
-    const [selectedTab, setSelectedTab] = useState('productos');
-    const navigate = useNavigate()
+  const [selectedTab, setSelectedTab] = useState("productos");
+  const navigate = useNavigate();
 
+  const handleTabClick = (tab) => {
+    setSelectedTab(tab);
+  };
 
-    const handleTabClick = (tab) => {
-        setSelectedTab(tab);
-      };
-    
-    return (
-        <div>
-            <SideBar handleTabClick = {handleTabClick} selectedTab = {selectedTab}/>
-            { selectedTab === 'insumos' ? (
+  return (
+    <div>
+      <SideBar handleTabClick={handleTabClick} selectedTab={selectedTab} />
+      {selectedTab === "insumos" ? (
         <TablaInsumos />
-      ) : 
-selectedTab === 'proveedores' ? (
+      ) : selectedTab === "proveedores" ? (
         <ProveedoresPage />
-      ) : null
-    }
-        </div>
-    )
+      ) : null}
+    </div>
+  );
 }
