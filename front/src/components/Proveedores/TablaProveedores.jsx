98a175ff96a694720ea87543554425c1b57bab65
facundocont <facundocont.fc@gmail.com>
2023-11-17 09:59:50 -0300

    Update errores de form de Cocciones y update tutoriales

--- front/src/components/Proveedores/TablaProveedores.jsx
+++ front/src/components/Proveedores/TablaProveedores.jsx
@@ -178,6 +178,7 @@ export const TablaProveedores = () => {
         { element: '.section-content', popover: { title: 'Proveedores', description: 'Aquí podrás ver todos los proveedores cargados', side: 'left', align: 'start' } },
         { element: '.button-on-table-modificar', popover: { title: 'Modificar', description: 'Puedes cambiar algún dato del proveedor si crees necesario', side: 'left', align: 'start' } },
         { element: '.button-on-table-baja', popover: { title: 'Dar de baja', description: 'También puedes darlo de baja', side: 'left', align: 'start' } },
+        { element: '.button-on-table-whatsapp', popover: { title: 'Enviar whatsapp', description: 'Aquí tienes un link directo para mandarle un whatsapp', side: 'left', align: 'start' } },
         { element: '.search-box', popover: { title: 'Buscar', description: 'Si no encuentras lo que buscas, puedes ingresar el nombre del proveedor para encontrarlo', side: 'right', align: 'start' } },
         { element: '.btn-create', popover: { title: 'Nuevo proveedor', description: 'También puedes ir a cargar un nuevo proveedor directamente!', side: 'right', align: 'start' } },
         { popover: { title: 'Eso es todo!', description: 'Ya puedes continuar' } },
